1ed009f57ac0abea37d1309f0fb134c8d7b01422
Geethkiran <geeth53xy@gmail.com>
2020-03-11 16:41:59 +0530

    [geeth]Add. harmonic number is found

---
+++ Repetition/HarmonicNumber.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+read -p "enter a number to get its harmonic: " num
+harmonic=0
+for (( i=1;i<=$num;i++ ))
+do
+    harmonic=$(echo "scale=2;1/$i+$harmonic"|bc)
+done
+echo "$harmonic"
