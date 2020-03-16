8b072b249f903f0c53394c48f555faf6c99f00fd
Geethkiran <geeth53xy@gmail.com>
2020-03-16 13:27:04 +0530

    [Geeth]Add. sanke and ladder game is designed

---
+++ logical/SnakeAndLadder.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -x
+
+echo "---------Welcome to SnakeLadder Game----------"
+
+GAME_WINNING_POSITION=100
+player_1_Position=0
+player_2_Position=0
+player_1_turns=0
+player_2_turns=0
+
+ladder(){ playerPosition=$1
+case $playerPosition in
+19)
+playerPosition=66
+;;
+32)
+playerPosition=53
+;;
+67)
+playerPosition=100
+;;
+73)
+playerPosition=93
+;;
+esac
+return "$playerPosition"
+}
+
+snake(){
+
+playerPosition=$1
+case $playerPosition in
+34)
+playerPosition=9
+;;
+52)
+playerPosition=20
+;;
+74)
+playerPosition=28
+;;
+esac
+return "$playerPosition"
+}
+
+playCheck(){
+	playerrolldice=$1
+	playerPosition=$2
+	checkPlayer=$((RANDOM%3))
+	if [ $checkPlayer -eq 0 ]
+	then
+		playerPosition=$playerPosition
+	elif [ $checkPlayer -eq 1 ]
+	then
+		playerPosition=$(($playerPosition+$playerrolldice))
+		if (( $playerPosition>100 ))
+		then
+		playerPosition=$(($playerPosition-$playerrolldice))
+		fi
+		ladder $playerPosition
+		playerPosition=$?
+	elif [ $checkPlayer -eq 2 ]
+	then
+		snake $playerPosition
+		playerPosition=$?
+	fi
+	return "$playerPosition"
+}
+
+while [ $player_1_Position -lt $GAME_WINNING_POSITION ] && [ $player_2_Position -lt $GAME_WINNING_POSITION ]
+do
+	player_1_rolldice=$(($((RANDOM%6))+1))
+	playCheck $player_1_rolldice $player_1_Position
+	player_1_Position=$?
+
+	player_2_rolldice=$(($((RANDOM%6))+1))
+	playCheck $player_2_rolldice $player_2_Position
+	player_2_Position=$?
+
+	((player_1_turns++))
+	((player_2_turns++))
+done
+
+if [ $player_1_Position -eq $GAME_WINNING_POSITION ]
+	then
+		echo "Playerone is Won and num of turns is $player_1_turns"
+else
+		echo "Playertwo is Won and num of turns is $player_2_turns"
+fi
